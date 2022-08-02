9c90f5816ba4bdd970cd1de154f589eed133a751
Alejae1998 <alejagr1998@gmail.com>
2022-08-01 17:24:57 -0700

    books.test all passing after deleting test.js, trying to work vertically

--- sql/setup.sql
+++ sql/setup.sql
@@ -1,36 +1,35 @@
 -- Use this file to define your SQL tables
 -- The SQL in this file will be executed when you run `npm run setup-db`
-DROP TABLE IF EXIST books CASCADE;
-DROP TABLE IF EXIST authors CASCADE;
-DROP TABLE IF EXIST books_authors CASCADE;
-
+DROP TABLE IF EXISTS books CASCADE;
+DROP TABLE IF EXISTS authors CASCADE;
+DROP TABLE IF EXISTS books_authors CASCADE;
 
 CREATE TABLE books (
-  id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-  title VARCHAR(280) NOT NULL,
-  released INT NOT NULL,
+    id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    title VARCHAR(280) NOT NULL,
+    released INT NOT NULL
 );
 
-INSER INTO books (
-  title,
-  released
+INSERT INTO books (
+    title,
+    released
 )
-VALUES 
-('Lord of the Things', 1922),
-('Of Mice and Men', 1999),
-('Harry Potter', 2000);
+VALUES
+    ('Lord of the Things', 1922),
+    ('Of Mice and Men', 1999),
+    ('Harry Potter', 2000);
 
 CREATE TABLE authors (
-  id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-  name VARCHAR(280) NOT NULL,
-  dob INT NOT NULL,
-  pob VARCHAR(280) NOT NULL
+    id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    name VARCHAR(280) NOT NULL,
+    dob INT NOT NULL,
+    pob VARCHAR(280) NOT NULL
 );
 
 INSERT INTO authors (
-  name,
-  dob,
-  pob
+    name,
+    dob,
+    pob
 )
 VALUES
     ('Arthur McArthur', 1959, 'Orange County'),
@@ -38,12 +37,12 @@ VALUES
     ('Another McTest', 1890, 'Miami');
 
 CREATE TABLE books_authors(
-  id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-  books_id BIGINT,
-  authors_id BIGINT
+    id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    books_id BIGINT,
+    authors_id BIGINT
 );
 
 INSERT INTO books_authors (books_id, authors_id) VALUES
-(1,1),
-(2,2),
-(3,3);
+    (1, 1),
+    (2, 2),
+    (3, 3);
